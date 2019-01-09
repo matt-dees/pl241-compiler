e5de753cae405e8b03752255d54848067456d999
Matt Dees <mdees@uci.edu>
2019-01-09 15:47:56 -0800

    WIP: First test case for VCG

--- test/TestVcgGen.cpp
+++ test/TestVcgGen.cpp
@@ -1,7 +1,12 @@
+#include "Instruction.h"
 #include "IrGenContext.h"
 #include "VcgGen.h"
 #include <catch.hpp>
 
 using namespace cs241c;
 
-TEST_CASE("Test VCG Graph Generation") { IrGenContext Context; }
+TEST_CASE("Test VCG Graph Generation") {
+  BasicBlock BB1 = BasicBlock();
+  BB1.Instructions.emplace_back(
+      NegInstruction(std::make_unique<Value>("X").get(), &BB1));
+}
