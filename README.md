0dc0ad9efda04a2dcf719b4b0d54aff448b769fe
Matt Dees <mdees@uci.edu>
2019-01-15 14:59:54 -0800

    Added reference to README and started DominatorTree class.

---
+++ README.md
@@ -0,0 +1,6 @@
+# CS241 Compiler Project
+
+## References
+
+### Dominator Tree Generation
+<https://www.cs.rice.edu/~keith/EMBED/dom.pdf>
---
+++ src/ir/DominatorTree.h
@@ -0,0 +1,10 @@
+#ifndef CS241C_IR_DOMINATORTREE_H
+#define CS241C_IR_DOMINATORTREE_H
+
+class DominatorTree {
+public:
+protected:
+private:
+};
+
+#endif
