32c457fea371f5f5d99aba2ec68d664e939ef340
Matt Dees <mdees@uci.edu>
2019-01-15 20:13:56 -0800

    WIP: Dominator Tree construction

--- src/ir/DominatorTree.cpp
+++ src/ir/DominatorTree.cpp
@@ -1,1 +1,19 @@
+#include "DominatorTree.h"
+#include <stack>
+#include <unordered_set>
 
+using namespace cs241c;
+DomNode::DomNode(BasicBlock *BB) : BB(BB), DominatedNodes({}) {}
+
+std::unique_ptr<DomNode> DominatorTree::buildDomTree(BasicBlock *Entry) {}
+
+void DominatorTree::postOrder(std::vector<BasicBlock *> *Output,
+                              cs241c::BasicBlock *Entry) {
+  if (Entry->Terminator->followingBlocks().empty()) {
+    Output->push_back(Entry);
+    return;
+  }
+  for (auto NextBlock : Entry->Terminator->followingBlocks()) {
+    postOrder(Output, NextBlock);
+  }
+}
--- src/ir/DominatorTree.h
+++ src/ir/DominatorTree.h
@@ -1,10 +1,31 @@
 #ifndef CS241C_IR_DOMINATORTREE_H
 #define CS241C_IR_DOMINATORTREE_H
 
-class DominatorTree {
+#include "BasicBlock.h"
+#include <unordered_map>
+
+namespace cs241c {
+
+class DomNode {
 public:
-protected:
-private:
+  DomNode(BasicBlock *BB);
+  BasicBlock *BB;
+  std::vector<std::unique_ptr<DomNode>> DominatedNodes;
 };
 
+class DominatorTree {
+public:
+  DominatorTree() {}
+
+  // Uses algorithm proposed by Cooper, Harvey, and Kennedy from Rice
+  // to build dominator tree.
+  // https://www.cs.rice.edu/~keith/EMBED/dom.pdf
+  std::unique_ptr<DomNode> buildDomTree(BasicBlock *Entry);
+
+protected:
+private:
+  static void postOrder(std::vector<BasicBlock *> *Output, BasicBlock *Entry);
+};
+
+} // namespace cs241c
 #endif
