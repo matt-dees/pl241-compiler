eda09731faea83031450c89db56503cf90bbe7ad
Fabian Parzefall <mail@fabianparzefall.de>
2019-02-08 16:38:46 -0800

    Fix segfault in dominator tree test

--- test/TestDomTreeGen.cpp
+++ test/TestDomTreeGen.cpp
@@ -9,60 +9,68 @@ using namespace std;
 TEST_CASE("Test Dominator Tree Generation 1") {
   auto CompilationUnit = make_unique<Module>("program");
   IrGenContext Context(CompilationUnit.get());
+
+  auto F = make_unique<Function>("main", vector<unique_ptr<LocalVariable>>{});
+  Context.declare(move(F));
+
   // Test with 4 BasicBlocks
-  unique_ptr<BasicBlock> BB1 = make_unique<BasicBlock>(Context.genBasicBlockName());
-  unique_ptr<BasicBlock> BB2 = make_unique<BasicBlock>(Context.genBasicBlockName());
-  unique_ptr<BasicBlock> BB3 = make_unique<BasicBlock>(Context.genBasicBlockName());
-  unique_ptr<BasicBlock> BB4 = make_unique<BasicBlock>(Context.genBasicBlockName());
+  BasicBlock *BB1 = Context.makeBasicBlock();
+  BasicBlock *BB2 = Context.makeBasicBlock();
+  BasicBlock *BB3 = Context.makeBasicBlock();
+  BasicBlock *BB4 = Context.makeBasicBlock();
 
-  Context.currentBlock() = BB1.get();
-  BB1->terminate(make_unique<BraInstruction>(Context.genInstructionId(), BB2.get()));
+  Context.currentBlock() = BB1;
+  BB1->terminate(make_unique<BraInstruction>(Context.genInstructionId(), BB2));
 
-  Context.currentBlock() = BB2.get();
-  BB2->terminate(make_unique<BraInstruction>(Context.genInstructionId(), BB3.get()));
+  Context.currentBlock() = BB2;
+  BB2->terminate(make_unique<BraInstruction>(Context.genInstructionId(), BB3));
 
-  Context.currentBlock() = BB3.get();
-  BB3->terminate(make_unique<BraInstruction>(Context.genInstructionId(), BB4.get()));
+  Context.currentBlock() = BB3;
+  BB3->terminate(make_unique<BraInstruction>(Context.genInstructionId(), BB4));
 
-  Context.currentBlock() = BB4.get();
+  Context.currentBlock() = BB4;
   BB4->terminate(make_unique<EndInstruction>(Context.genInstructionId()));
 
   DominatorTree D;
-  D.buildDominatorTree(BB1.get());
+  D.buildDominatorTree(BB1);
   CHECK(true);
 }
 
 TEST_CASE("Test Dominator Tree Generation 2") {
   auto CompilationUnit = make_unique<Module>("program");
   IrGenContext Context(CompilationUnit.get());
+
+  auto F = make_unique<Function>("main", vector<unique_ptr<LocalVariable>>{});
+  Context.declare(move(F));
+
   // Test with 4 BasicBlocks
-  unique_ptr<BasicBlock> BB1 = make_unique<BasicBlock>(Context.genBasicBlockName());
-  unique_ptr<BasicBlock> BB2 = make_unique<BasicBlock>(Context.genBasicBlockName());
-  unique_ptr<BasicBlock> BB3 = make_unique<BasicBlock>(Context.genBasicBlockName());
-  unique_ptr<BasicBlock> BB4 = make_unique<BasicBlock>(Context.genBasicBlockName());
-  unique_ptr<BasicBlock> BB5 = make_unique<BasicBlock>(Context.genBasicBlockName());
+  BasicBlock *BB1 = Context.makeBasicBlock();
+  BasicBlock *BB2 = Context.makeBasicBlock();
+  BasicBlock *BB3 = Context.makeBasicBlock();
+  BasicBlock *BB4 = Context.makeBasicBlock();
+  BasicBlock *BB5 = Context.makeBasicBlock();
 
-  Context.currentBlock() = BB1.get();
-  BB1->terminate(make_unique<BraInstruction>(Context.genInstructionId(), BB2.get()));
+  Context.currentBlock() = BB1;
+  BB1->terminate(make_unique<BraInstruction>(Context.genInstructionId(), BB2));
 
-  Context.currentBlock() = BB2.get();
+  Context.currentBlock() = BB2;
   auto Cmp = make_unique<CmpInstruction>(Context.genInstructionId(), Context.makeConstant(5), Context.makeConstant(5));
   CmpInstruction *CmpP = Cmp.get();
   BB2->appendInstruction(move(Cmp));
 
-  BB2->terminate(make_unique<BneInstruction>(Context.genInstructionId(), CmpP, BB3.get(), BB4.get()));
+  BB2->terminate(make_unique<BneInstruction>(Context.genInstructionId(), CmpP, BB3, BB4));
 
-  Context.currentBlock() = BB3.get();
+  Context.currentBlock() = BB3;
 
-  BB3->terminate(make_unique<BraInstruction>(Context.genInstructionId(), BB5.get()));
+  BB3->terminate(make_unique<BraInstruction>(Context.genInstructionId(), BB5));
 
-  Context.currentBlock() = BB4.get();
+  Context.currentBlock() = BB4;
 
-  BB4->terminate(make_unique<BraInstruction>(Context.genInstructionId(), BB5.get()));
+  BB4->terminate(make_unique<BraInstruction>(Context.genInstructionId(), BB5));
 
-  Context.currentBlock() = BB5.get();
+  Context.currentBlock() = BB5;
   BB5->terminate(make_unique<EndInstruction>(Context.genInstructionId()));
   DominatorTree D;
-  D.buildDominatorTree(BB1.get());
+  D.buildDominatorTree(BB1);
   CHECK(true);
 }
