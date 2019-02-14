817a2be266cc3c14fb7a3638cdd64e4dc4740e6a
Fabian Parzefall <mail@fabianparzefall.de>
2019-02-14 13:27:41 -0800

    Add virtual destructor to Vcg

--- src/vcg/Vcg.h
+++ src/vcg/Vcg.h
@@ -1,17 +1,20 @@
-#ifndef CS241C_VCGWRITABLE_H
-#define CS241C_VCGWRITABLE_H
+#ifndef CS241C_VCG_VCGWRITABLE_H
+#define CS241C_VCG_VCGWRITABLE_H
+
 #include <fstream>
 #include <iostream>
 #include <string>
+
 namespace cs241c {
 class Vcg {
 protected:
   virtual void writeGraph(std::ofstream &OutFileStream) = 0;
-
   void writeProperties(std::ofstream &OutFileStream);
 
 public:
+  virtual ~Vcg() = default;
   void writeToFile(const std::string &OutFilePath);
 };
 } // namespace cs241c
-#endif // CS241C_VCGWRITABLE_H
+
+#endif
