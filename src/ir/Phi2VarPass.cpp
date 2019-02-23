4bd6fbea9cee89eb5ef1e874f4436f9c38732bc7
Fabian Parzefall <mail@fabianparzefall.de>
2019-02-23 15:35:11 -0800

    Fix Phi2Var by separating analysis and substitution

--- src/ir/Phi2VarPass.cpp
+++ src/ir/Phi2VarPass.cpp
@@ -13,17 +13,20 @@ void Phi2VarPass::process(Function &F) {
   auto Registers = FA.coloring(&F);
   map<ValueRef, ValueRef> PhiSubstitutions;
 
-  auto Blocks = F.postOrderCfg();
-  reverse(Blocks.begin(), Blocks.end());
-  for (auto BB : Blocks) {
+  for (auto &BBPtr : F.basicBlocks()) {
+    auto BB = BBPtr.get();
+
     auto &Predecessors = BB->predecessors();
 
     auto &Instructions = BB->instructions();
     auto InstructionsEnd = Instructions.end();
-    auto InstrIt = Instructions.begin();
-    for (; InstrIt != InstructionsEnd; ++InstrIt) {
+
+    // 1. First push all the move instructions to the parents.
+    for (auto InstrIt = Instructions.begin(); InstrIt != InstructionsEnd; ++InstrIt) {
       auto &Instr = *InstrIt;
 
+      // Phi instructions are only in the very beginning of the block, so it is safe to break once a non phi instruction
+      // is encountered.
       if (Instr->InstrT != InstructionType::Phi)
         break;
 
@@ -35,33 +38,33 @@ void Phi2VarPass::process(Function &F) {
       PhiSubstitutions[Instr.get()] = Target;
 
       auto Args = Instr->arguments();
+
       for (int I = 0; I < 2; ++I) {
         auto Arg = Args[I];
         auto ArgRegister = Registers->find(Arg) != Registers->end() ? Registers->at(Arg) : RegisterAllocator::SPILL;
+
         if (ArgRegister != TargetRegister) {
           ValueRef Source =
               ArgRegister != RegisterAllocator::RA_REGISTER::SPILL ? ValueRef(ValueType::Register, ArgRegister) : Arg;
           auto Move = make_unique<MoveInstruction>(NameGen::genInstructionId(), Source, Target);
           Move->storage() = Instr->storage();
 
-          auto TargetPredecessor = Predecessors[I];
-          if (TargetPredecessor->successors().size() > 1) {
-            auto IntermediateBB = make_unique<BasicBlock>(NameGen::genBasicBlockName());
-            TargetPredecessor->updateSuccessor(BB, IntermediateBB.get());
-            IntermediateBB->fallthoughSuccessor() = BB;
-            TargetPredecessor = IntermediateBB.get();
-            F.basicBlocks().push_back(move(IntermediateBB));
-          }
-
           Predecessors[I]->appendInstruction(move(Move));
         }
       }
     }
-    Instructions.erase(Instructions.begin(), InstrIt);
+  }
 
+  // 2. Update all arguments and remove phi instructions.
+  for (auto &BBPtr : F.basicBlocks()) {
+    auto &Instructions = BBPtr->instructions();
     for (auto &Instr : Instructions) {
       Instr->updateArgs(PhiSubstitutions);
     }
+
+    Instructions.erase(remove_if(Instructions.begin(), Instructions.end(),
+                                 [](const auto &Instr) { return Instr->InstrT == InstructionType::Phi; }),
+                       Instructions.end());
   }
 }
 
